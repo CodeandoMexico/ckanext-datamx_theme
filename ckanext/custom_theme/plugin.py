20da6b73b47521f31d01dc3a2e8859589056dc0a
Braulio Chavez <pataslargas27@gmail.com>
2013-11-21 14:31:58 -0800

    Adds public folder for public static files

--- ckanext/custom_theme/plugin.py
+++ ckanext/custom_theme/plugin.py
@@ -10,3 +10,7 @@ class CustomThemePlugin(plugins.SingletonPlugin):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
         toolkit.add_template_directory(config, 'templates')
+
+        # Add this plugin's public dir to CKAN's extra_public_patsh, so
+        # that CKAN will use this plugin's custom static files.
+        toolkit.add_public_directory(config, 'public')
