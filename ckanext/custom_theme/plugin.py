3caf0a3ce10b88e33b32bc5d791e0a0301da8fec
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-03-12 03:09:08 -0600

    Retrieves and show information of Apps related with an organization

--- ckanext/custom_theme/plugin.py
+++ ckanext/custom_theme/plugin.py
@@ -14,3 +14,10 @@ class CustomThemePlugin(plugins.SingletonPlugin):
         # Add this plugin's public dir to CKAN's extra_public_patsh, so
         # that CKAN will use this plugin's custom static files.
         toolkit.add_public_directory(config, 'public')
+
+        # Register this plugin's fanstatic directory with CKAN.
+        # Here, 'fanstatic' is the path to the fanstatic directory
+        # (relative to this plugin.py file), and 'example_theme' is the name
+        # that we'll use to refer to this fanstatic directory from CKAN
+        # templates.
+        toolkit.add_resource('fanstatic', 'custom_theme')
